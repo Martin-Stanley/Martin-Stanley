5f21b95bd44cd21d854ccbbd1c4efd801d7f0fff
Martin-Stanley <105112038+Martin-Stanley@users.noreply.github.com>
2022-05-09 23:17:00 +0200

    Add files via upload

---
+++ Adventure.py
@@ -0,0 +1,28 @@
+name = input("Type your name: ")
+print("Welcome", name, " to this adventure! ")
+
+answer = input\
+    ("You are on the dirt road, it has come to an end and you can go left or right, which one you want to go? " ).lower()
+if answer == "left":
+        answer = input("You come to a river, you can walk around it or swim accros? Type walk to walk around and swim to swim across. ")
+        if answer == "swim":
+            print("You swim across and were eaten by the alligator.")
+        elif answer == "walk":
+            print("You walked for many miles, ran out of the water and you lost the game. ")
+        else:
+            print("Not a valid option. You lose.")
+
+elif answer == "right":
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back) ? ")
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        print("You cross the bridge and met a stranger. Do you talk to them (yes/no)?  ")
+        if answer == "yes":
+            print("Yes talk to the stranger and they give you gold. You Win! ")
+        elif answer == "no":
+            print("You ignore the stranger and they are offended and you lose. ")
+    else:
+        print("Not a valid option. You lose.")
+else:
+    print("Not a valid option. You lose.")
